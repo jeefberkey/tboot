2a7d3803d7c23781884ff558955f5f4b0f25614f
Nick Miller <nick.miller@onyxpoint.com>
2017-07-25 10:41:37 -0400

    trailing colons

--- create-lcp-tboot-policy_el7.sh
+++ create-lcp-tboot-policy_el7.sh
@@ -52,10 +52,10 @@ if [ ! -e /boot/tboot.gz ]; then
     exit 1
 fi
 
-echo "a. Create hash for tboot.gz and store it in the mle_hash file:"
+echo "a. Create hash for tboot.gz and store it in the mle_hash file"
 lcp_mlehash -c "${GRUB_CMDLINE_TBOOT}" /boot/tboot.gz > mle_hash
 
-echo "b. Create the policy element for tboot (“mle”), take the input hash from mle_hash, and output to mle_elt:"
+echo "b. Create the policy element for tboot (“mle”), take the input hash from mle_hash, and output to mle_elt"
 lcp_crtpolelt --create --type mle --ctrl 0x00 --minver 17 --out mle.elt mle_hash
 
 echo "c. Create the policy element for the platform configuration (“pconf”)."
@@ -65,16 +65,16 @@ lcp_crtpolelt --create --type pconf --out pconf.elt pcrs
 echo "d. Create the unsigned policy list file list_unsig.lst, using mle_elt and pconf_elt"
 lcp_crtpollist --create --out list_unsig.lst mle.elt pconf.elt
 
-echo "e. Create an RSA key pair, and use it to sign the policy list, list_sig.lst, in both the input and the output files:"
+echo "e. Create an RSA key pair, and use it to sign the policy list, list_sig.lst, in both the input and the output files"
 openssl genrsa -out privkey.pem 2048 &> /dev/null
 openssl rsa -pubout -in privkey.pem -out pubkey.pem &> /dev/null
 cp list_unsig.lst list_sig.lst
 lcp_crtpollist --sign --pub pubkey.pem --priv privkey.pem --out list_sig.lst
 
-echo "f. Create the final LCP policy blobs from list_sig.lst, and generate the list_pol and list_data files:"
+echo "f. Create the final LCP policy blobs from list_sig.lst, and generate the list_pol and list_data files"
 lcp_crtpol2 --create --type list --pol list.pol --data list.data list_sig.lst
 
-echo "g. Generate the tboot policy to control expected kernel and initrd:"
+echo "g. Generate the tboot policy to control expected kernel and initrd"
 tb_polgen --create --type nonfatal vl.pol
 
 # set the boot CMD_LINE
@@ -93,22 +93,22 @@ INITRAMFS_IMG=`ls /boot/ | grep initramfs-[23] | head -n1`
 tb_polgen --add --num 0 --pcr none --hash image --cmdline "$CMD_LINE" --image /boot/${KERNEL_IMG} vl.pol
 tb_polgen --add --num 1 --pcr 19 --hash image --cmdline "" --image /boot/${INITRAMFS_IMG} vl.pol
 
-echo "h. Create the TPM NV index for recording boot errors:"
+echo "h. Create the TPM NV index for recording boot errors"
 tpmnv_defindex -i 0x20000002 -s 8 -pv 0 -rl 0x07 -wl 0x07 -p "$PASSWORD"
 
-echo "i. Create the TPM NV index for the owner-created LCP policy:"
+echo "i. Create the TPM NV index for the owner-created LCP policy"
 tpmnv_defindex -i owner -p "$PASSWORD"
 
-echo "j. Create the TPM NV index for the tboot policy:"
+echo "j. Create the TPM NV index for the tboot policy"
 tpmnv_defindex -i 0x20000001 -s 256 -pv 0x02 -p "$PASSWORD"
 
 echo "k. Write the LCP policy (from list.pol) into the owners NV index"
 lcp_writepol -i owner -f list.pol -p "$PASSWORD"
 
-echo "l. Write the tboot policy (from vl.pol) into the NV index:"
+echo "l. Write the tboot policy (from vl.pol) into the NV index"
 lcp_writepol -i 0x20000001 -f vl.pol -p "$PASSWORD"
 
-echo "m. Copy list data to /boot/ for use by GRUB:"
+echo "m. Copy list data to /boot/ for use by GRUB"
 cp list.data /boot/
 
 echo "--------------------------------------------------------------------------------"
